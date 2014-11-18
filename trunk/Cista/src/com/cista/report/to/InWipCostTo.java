29e7dd111c76ea326c17a39e4affc4324a5d2ff4
tunghank@gmail.com <tunghank@gmail.com@96f835c8-c2b6-2603-ddf4-4a5d8e0df1a9>
2014-11-18 08:01:28 +0000

    Cista Report

---
+++ trunk/Cista/src/com/cista/report/to/InWipCostTo.java
@@ -0,0 +1,67 @@
+package com.cista.report.to;
+
+/**
+ * SysFunction entity.
+ * 
+ * @author MyEclipse Persistence Tools
+ */
+
+public class InWipCostTo extends com.cista.system.util.BaseObject 
+implements	java.io.Serializable 
+{
+	// Fields
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private String ta001;
+	private String product;
+	private Double materialCost;
+
+
+	
+	// Constructors
+
+	/** default constructor */
+	public InWipCostTo() {
+	}
+
+
+
+	public String getTa001() {
+		return ta001;
+	}
+
+
+
+	public void setTa001(String ta001) {
+		this.ta001 = ta001;
+	}
+
+
+
+	public String getProduct() {
+		return product;
+	}
+
+
+
+	public void setProduct(String product) {
+		this.product = product;
+	}
+
+
+
+	public Double getMaterialCost() {
+		return materialCost;
+	}
+
+
+
+	public void setMaterialCost(Double materialCost) {
+		this.materialCost = materialCost;
+	}
+
+
+}
