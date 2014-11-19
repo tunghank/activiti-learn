323e3f2df2970643b50d090bd7a7fc861b23f3a7
tunghank@gmail.com <tunghank@gmail.com@96f835c8-c2b6-2603-ddf4-4a5d8e0df1a9>
2014-11-19 10:32:33 +0000

    Cista Report

--- trunk/Cista/src/com/cista/report/dao/InWipCostDao.java
+++ trunk/Cista/src/com/cista/report/dao/InWipCostDao.java
@@ -21,8 +21,7 @@ public class InWipCostDao extends CistaERPBaseDao{
 		
 		SimpleJdbcTemplate sjt = getSimpleJdbcTemplate();
 		String sql  = " SELECT SUBSTRING(A.TA001,0,4) TA001 , SUBSTRING(MB003,0,6) PRODUCT, " +
-					" ( ISNULL(SUM(A.TA026),0) + ISNULL(SUM(A.TA029),0) ) AS MATERIAL_COST, " +
-					" ( 0- ( ISNULL(SUM(A.TA036),0) + ISNULL(SUM(A.TA039),0) ) ) AS NG_COST " +
+					" ( ISNULL(SUM(A.TA026),0) + ISNULL(SUM(A.TA029),0) ) AS MATERIAL_COST " +
 					" FROM CISTA_HK..CSTTA AS A " +
 					" JOIN CISTA_HK..MOCTA AS B ON A.TA003=B.TA001 AND A.TA004=B.TA002  " +
 					" LEFT JOIN CISTA_HK..INVMB AS C ON A.TA001=MB001 " +
@@ -46,4 +45,32 @@ public class InWipCostDao extends CistaERPBaseDao{
 		}	
 	}
 
+	public List<InWipCostTo> getNgCostByProduct(String edate, String product) throws DataAccessException{
+		
+		SimpleJdbcTemplate sjt = getSimpleJdbcTemplate();
+		String sql  = " SELECT SUBSTRING(A.TA001,0,4) TA001 , SUBSTRING(MB003,0,6) PRODUCT, " +
+					" ( 0- ( ISNULL(SUM(A.TA036),0) + ISNULL(SUM(A.TA039),0) ) ) AS NG_COST " +
+					" FROM CISTA_HK..CSTTA AS A " +
+					" JOIN CISTA_HK..MOCTA AS B ON A.TA003=B.TA001 AND A.TA004=B.TA002  " +
+					" LEFT JOIN CISTA_HK..INVMB AS C ON A.TA001=MB001 " +
+					" WHERE 1=1  " +
+					//" AND ((A.TA023<>0 OR A.TA024<>0 OR A.TA031<>0) AND (A.TA002='" + month + "')) " +
+					" and  A.TA002 <= '" + edate + "' " +
+					" AND  MB003 LIKE '" + product + "%' " +
+					" GROUP BY SUBSTRING(A.TA001,0,4) , SUBSTRING(MB003,0,6) " +
+					" ORDER BY 1 ";
+								
+    	ParameterizedBeanPropertyRowMapper<InWipCostTo> rowMapper = 
+    		new ParameterizedBeanPropertyRowMapper<InWipCostTo>();
+    	rowMapper.setMappedClass(InWipCostTo.class);
+
+    	List<InWipCostTo> result = sjt.query(sql,rowMapper, new Object[] {} );
+
+		logger.debug("SQL " + sql);
+		if (result != null && result.size() > 0) {
+			return result;
+		} else {
+			return null;
+		}	
+	}
 }
