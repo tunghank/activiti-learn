646838b9918c325ba0a2ac057d2e0f25b74fec0e
tunghank <tunghank@gmail.com>
2014-12-17 09:01:58 +0000

    Foundry Wip

--- Cista/src/com/cista/report/action/FoundryWip.java
+++ Cista/src/com/cista/report/action/FoundryWip.java
@@ -3,6 +3,7 @@ package com.cista.report.action;
 
 import java.io.File;
 import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
@@ -12,7 +13,12 @@ import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletResponse;
 import org.apache.struts2.ServletActionContext;
 
+import com.cista.report.dao.FoundryWipDao;
+import com.cista.system.to.ExtJSGridTo;
+import com.cista.report.to.FoundryWipTo;
 import com.cista.system.util.BaseAction;
+import com.cista.system.util.CistaUtil;
+import com.google.gson.Gson;
 
 
 //JExcel
@@ -46,21 +52,97 @@ public class FoundryWip extends BaseAction{
 		return SUCCESS;
 	}
 	
-	public String CreateFoundryWip() throws Exception{
+	public String QueryFoundryWip() throws Exception{
+		
 		try {
 			request= ServletActionContext.getRequest();
-			response= ServletActionContext.getResponse();
+			//for paging        
+	        int total;//分頁。。。數據總數       
+	        int iStart;//分頁。。。每頁開始數據
+	        int iLimit;//分頁。。。每一頁數據
+	        
+	        String start = request.getParameter("start");
+            String limit = request.getParameter("limit");
+            iStart = Integer.parseInt(start);
+            iLimit = Integer.parseInt(limit);
+            
+            logger.debug("start: " + Integer.parseInt(start));
+            logger.debug("limit: " + limit);
 			
-	        
-	        
-	        return SUCCESS;
+            
+			String lot = request.getParameter("lot"); 
+			lot = null != lot ? lot : "";
+			logger.debug("lot " + lot);
+			
+			String cistaProject = request.getParameter("cistaProject"); 
+			cistaProject = null != cistaProject ? cistaProject : "";
+			logger.debug("cistaProject " + cistaProject);
+			
+			String vendorCode = "10001";
+			
+			FoundryWipDao foundryWipDao = new FoundryWipDao();		
+	
+			List<FoundryWipTo> wipList = foundryWipDao.getFountryWip(vendorCode, lot, cistaProject);
+			
+			
+			//logger.debug("userList Size " + userList.size());
+			//分頁
+			if( wipList!= null){
+				total=wipList.size();
+				logger.debug("total " + total);
+				
+				int end=iStart+iLimit;
+				if(end>total){//不能總數
+					end=total;
+				}		
+				
+				List<FoundryWipTo> resultList = new ArrayList<FoundryWipTo>();
+				for(int i=iStart;i<end;i++){//只加載當前頁面數據
+					//logger.debug(userList.get(i).toString());
+					resultList.add(wipList.get(i));
+				}
+				
+				
+				//logger.debug(resultList.toString());
+				
+				ExtJSGridTo extJSGridTo = new ExtJSGridTo();
+				extJSGridTo.setTotal(total);
+				extJSGridTo.setRoot(resultList);
+				
+				Gson gson = new Gson();
+				String jsonData = gson.toJson(extJSGridTo);
+				//logger.debug(jsonData);
+				
+				// 1.5 Set AJAX response
+				CistaUtil.ajaxResponseData(response, jsonData);
+			}else{
+				CistaUtil.ajaxResponseData(response, "");
+			}
+
 		} catch (Exception e) {
 			this.addActionMessage("ERROR");
 			e.printStackTrace();
 			logger.error(e.toString());
 			addActionMessage(e.toString());
-			return ERROR;
+          	//AJAX
+          	try{
+  		    	response.setContentType("text/html; charset=UTF-8");
+  				PrintWriter out = response.getWriter();
+  				String returnResult = "ERROR" ;
+
+  				logger.debug(returnResult);
+  				logger.debug("Error");
+  				out.print(returnResult);
+  				out.close();
+          	}catch(Exception ex){
+          		ex.printStackTrace();
+                logger.error(ex.toString());
+                return NONE;
+          	}
+
 		}
+		
+   		return NONE;
 		
 	}
 	
---
+++ Cista/src/com/cista/report/dao/FoundryWipDao.java
@@ -0,0 +1,62 @@
+package com.cista.report.dao;
+
+import java.util.List;
+
+
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.simple.ParameterizedBeanPropertyRowMapper;
+import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
+
+import com.cista.report.to.FoundryWipTo;
+import com.cista.system.util.BaseDao;
+
+
+public class FoundryWipDao extends BaseDao{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+
+
+	public List<FoundryWipTo> getFountryWip(String vendorCode, String lot, String cistaProject) throws DataAccessException{
+		
+		SimpleJdbcTemplate sjt = getSimpleJdbcTemplate();
+		
+		String sql  = " SELECT A.FOUNDRY_WIP_UUID, A.VENDOR_CODE, A.VENDOR, A.VENDOR_SITE_NUM, " +
+				" A.PROCESS, A.CISTA_PO, A.VENDOR_PROD, A.CISTA_PART_NUM, " +
+				" A.CISTA_PROJECT, A.WAFER_LOT_ID, A.VENDOR_LOT_ID, A.WAFER_QTY, " +
+				" A.LOT_TYPE, A.TOTAL_LAYER, A.REMAIN_LAYER, A.LOT_STATUS, " +
+				" A.CURR_HOLD_DAY, A.HOLD_CODE, A.HOLD_REAS, A.PRIORITY, " +
+				" A.WAFER_START, A.CURR_STAGE, A.STG_IN_DATE, A.SOD, A.RSOD, " +
+				" A.RPT_DATE, A.SHIP_TO, A.CDT " +
+				" FROM RPT_FOUNDRY_WIP A" +
+				" WHERE 1=1 ";
+		
+				if(vendorCode != null && !vendorCode.equals("") ){
+					sql = sql + " AND A.VENDOR_CODE = '" + vendorCode + "'";
+				}
+				if(lot != null && !lot.equals("") ){
+					sql = sql + " AND A.WAFER_LOT_ID = '" + lot + "'";
+				}
+				if(cistaProject != null && !cistaProject.equals("") ){
+					sql = sql + " AND A.CISTA_PROJECT = '" + cistaProject + "'";
+				}
+				
+				sql = sql + " ORDER BY CISTA_PROJECT, WAFER_LOT_ID" ;
+		
+				logger.debug("sql " + sql );
+    	ParameterizedBeanPropertyRowMapper<FoundryWipTo> rowMapper = 
+    		new ParameterizedBeanPropertyRowMapper<FoundryWipTo>();
+    	rowMapper.setMappedClass(FoundryWipTo.class);
+
+    	List<FoundryWipTo> result = sjt.query(sql,rowMapper, new Object[] {} );
+		
+		if (result != null && result.size() > 0) {
+			return result;
+		} else {
+			return null;
+		}	
+	}
+}
